1274c52247a7f77517f960a6e152a8066ca0ad59
luyuh <774964107@qq.com>
2021-08-17 14:57:47 +0800

    add more epoll

--- network/epoll.h
+++ network/epoll.h
@@ -4,10 +4,29 @@
 
 #ifndef REDIS_CLIENT_EPOLL_H
 #define REDIS_CLIENT_EPOLL_H
-
+#include <sys/epoll.h>
 
 class Epoll {
+public:
+    Epoll() : size_(512)
+    {
+        fd_ = epoll_create(size_);
+    }
 
+    Epoll(int size)
+    {
+        size_ = size;
+        fd_ = epoll_create(size);
+    }
+
+    void Add();
+    void Delete();
+    void Modify();
+    void Wait();
+
+private:
+    int fd_;
+    int size_;
 };
 
 
