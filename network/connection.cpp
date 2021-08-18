5d23f6f899a2e5e7bcb48034ca487f30b6d1fbf6
luyuh <774964107@qq.com>
2021-08-18 14:21:42 +0800

    add connection class

---
+++ network/connection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by admin on 2021/8/18.
+//
+
+#include "connection.h"
---
+++ network/connection.h
@@ -0,0 +1,14 @@
+//
+// Created by admin on 2021/8/18.
+//
+
+#ifndef NETWORK_CONNECTION_H
+#define NETWORK_CONNECTION_H
+
+
+class connection {
+
+};
+
+
+#endif //NETWORK_CONNECTION_H
